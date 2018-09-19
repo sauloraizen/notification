af10cf0244824aafb445f26f32bb8f5e2c7db917
Saulo Meneghel <saulomeneghel@MacBook-Air-de-Saulo.local>
2018-09-19 20:28:27 -0300

    Library ok Aula: Classe de Envio de E-mail

--- _app/email.php
+++ _app/email.php
@@ -7,23 +7,23 @@ class Email{
     
     private $mail = \stdClass::class;
 
-    public function __construct() { 
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName) { 
         
         //Server settings
         $this->mail = new PHPMailer(true);    
-        $this->mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $this->mail->SMTPDebug = $smtpDebug;                                 // Enable verbose debug output
         $this->mail->isSMTP();                                      // Set mailer to use SMTP
-        $this->mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+        $this->mail->Host = $host;  // Specify main and backup SMTP servers
         $this->mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $this->mail->Username = 'user@example.com';                 // SMTP username
-        $this->mail->Password = 'secret';                           // SMTP password
-        $this->mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $this->mail->Port = 587;    
+        $this->mail->Username = $user;                 // SMTP username
+        $this->mail->Password = $pass;                           // SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;                            // Enable TLS encryption, `ssl` also accepted
+        $this->mail->Port = $port;    
         $this->mail->CharSet = 'utf-8';
         $this->mail->setLanguage('br');
         
         //Recipients
-        $this->mail->setFrom('from@example.com', 'Name User Mailer');
+        $this->mail->setFrom($setFromEmail, $setFromName);
     }
 
     public function sendMail($subject, $body, $replyEmail, $replayName, $addressEmail, $addressName) {
--- test/index.php
+++ test/index.php
@@ -1,13 +1,13 @@
 <?php
 
-require __DIR__ . '/lib_ext/autoload.php'; //faz o autoload do composer
+require __DIR__ . '/../lib_ext/autoload.php'; //faz o autoload do composer
 
 //FORMA 1
 //use Notification\Email;
 //$novoEmail = new Email;
 
 //FORMA 2
-$novoEmail = new Notification\Email;
+$novoEmail = new Notification\Email(2, 'mail.gruporaizen.com.br', 'contato@gruporaizen.com.br', 'graizen_4573', 'tsl', 465, 'contato@gruporaizen.com.br', 'Nome do User');
 
 $novoEmail->sendMail("assunto de Teste", "<p>Esse é um -email de <b>teste!</b></p>", "emailreply@teste.com", "Nome do User", "emaildestinario@mail.com" ,"Nome do Destinario");
 
